ef1550edc0c7e83b823cfa97e4f94407ee795a99
AdiPodlas <a.podlasinski@icloud.com>
2024-06-11 18:05:29 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import {BlogEntry} from './BlogEntry'
 
 
 function App() {
   return (
     <div>
-    <div className="App-header">
-      Nagłówek
+    <div className="app-header">
+      Podróże kulinarne
+          {/* <br/> string od nowej linijki */}
+      
     </div>
     <div className="content">
       Treść tutaj
+      <BlogEntry>
+
+      </BlogEntry>
     </div>
   </div>
 );
---
+++ src/BlogEntry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BlogEntry.css';
+
+
+export const BlogEntry = ({ title }) => {
+    return (
+        <header className="blog-entry">
+            <h1>{title}
+                xyz
+            </h1>
+        </header>
+    );
+};
+
