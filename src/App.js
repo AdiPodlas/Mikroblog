bf2844e5a49e2934b92936bc4d906ff48fcc2ed3
AdiPodlas <a.podlasinski@icloud.com>
2024-09-02 19:21:08 +0200

    button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
-import React, { useState } from "react";
-import MainPage from "./MainPage";
-import { BlogEntry } from "./BlogEntry";
-import { NewEntryForm } from "./NewEntryForm";
-import { useEntries } from "./useEntries";
-import { PaginationControls } from "./PaginationControls";
+import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./MainPage";
 import AboutPage from "./AboutPage";
-
 
 function App() {
   return (
@@ -15,25 +10,23 @@ function App() {
       <div className="app-header">Podróże kulinarne</div>
 
       <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/about"> O nas </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className="navigation">
+          <ul className="nav-links">
+            <li>
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/about">O nas</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="content">
-        
+        <div className="content">
           <Routes>
             <Route index element={<MainPage />} />
             <Route path="about" element={<AboutPage />} />
           </Routes>
-        
-      </div>
+        </div>
       </BrowserRouter>
     </div>
   );
--- src/NewEntryForm.js
+++ src/NewEntryForm.js
@@ -1,69 +1,69 @@
-import React, { useState } from 'react';
-import './NewEntryForm.css';
-
-
+import React, { useState } from "react";
+import "./NewEntryForm.css";
 
 export const NewEntryForm = ({ onAddEntry }) => {
-    const [text, setText] = useState('');
-    const [author, setAuthor] = useState('');
-    const [isFocused, setIsFocused] = useState(false);
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
 
-    const handleTextChange = (e) => {
-        setText(e.target.value);
-    };
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleAuthorChange = (f) => {
-        setAuthor(f.target.value);
-    };
+  const handleAuthorChange = (f) => {
+    setAuthor(f.target.value);
+  };
 
-    const handleAddEntry = () => {
-        if (text.trim() !== '' & author.trim() !== '') {
-            onAddEntry(text, author);
-            setText(''); // Czyszczenie inputa po dodaniu wpisu
-            setAuthor('');
-        }
-    };
+  const handleAddEntry = () => {
+    if ((text.trim() !== "") & (author.trim() !== "")) {
+      onAddEntry(text, author);
+      setText(""); // Czyszczenie inputa po dodaniu wpisu
+      setAuthor("");
+    }
+  };
 
-    const handleFocus = () => {
-        setIsFocused(true);
-    };
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
 
-    const handleBlur = () => {
-        if (text.trim() === '') {
-            setIsFocused(false);
-        }
-    };
+  const handleBlur = () => {
+    if (text.trim() === "") {
+      setIsFocused(false);
+    }
+  };
 
-    const isFormValid = text.trim() !== '' && author.trim() !== '';
+  const isFormValid = text.trim() !== "" && author.trim() !== "";
 
-           return (
-            <div className="new-entry-form">
-                
-                <textarea
-                    value={text}
-                    onChange={handleTextChange}
-                    placeholder="Co u Ciebie słychać?"
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className={`new-entry-input ${isFocused || text.trim() !== '' ? 'focused' : ''}`}
-                />
-                <label className="new-entry-author-label">Autor: 
-                <input
-                    type="text"
-                    value={author}
-                    onChange={handleAuthorChange}
-                    className= "new-entry-author-input"   
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-                </label>
-                <button 
-                onClick={handleAddEntry} 
-                className="add-entry-button"
-                disabled={!isFormValid}
-                >
-                    Dodaj wpis
-                </button>
-            </div>
-        );
+  return (
+    <div className="new-entry-form">
+      <textarea
+        value={text}
+        onChange={handleTextChange}
+        placeholder="Co u Ciebie słychać?"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={`new-entry-input ${
+          isFocused || text.trim() !== "" ? "focused" : ""
+        }`}
+      />
+      <label className="new-entry-author-label">
+        Autor:
+        <input
+          type="text"
+          value={author}
+          onChange={handleAuthorChange}
+          className="new-entry-author-input"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+        <button
+          onClick={handleAddEntry}
+          className="add-entry-button"
+          disabled={!isFormValid}
+        >
+          Dodaj wpis
+        </button>
+      </label>
+    </div>
+  );
 };
