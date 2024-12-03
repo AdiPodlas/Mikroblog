359858d285269a413df8d76d5d7a5ba0b3db66bf
AdiPodlas <a.podlasinski@icloud.com>
2024-12-03 18:12:08 +0100

    fetchv2

--- src/useSearchEntries.js
+++ src/useSearchEntries.js
@@ -1,8 +1,24 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getEntries } from "./EntryRepository";
 
+
 export const useSearchEntries = (searchQuery) => {
-    const [entries] = useState(getEntries());
-    return     entries.filter (entry => entry.text.toUpperCase().includes(searchQuery.toUpperCase())) /* toUpperCase w dwóch miejscach tak aby wyszukiwanie pomijało wielkość liter, w tym przypadku wszystko formatujemy do dużych liter i elo, można też dać do małych */
+    const [entries, setEntries] = useState([]);
+    
+    const  fetchEntries = async() => {
+        const response = await fetch("http://localhost:8080/search?q=" + searchQuery)
+        const responseBody = await response.json()
+        setEntries (responseBody)
+       }
+
+       useEffect(() => {
+        fetchEntries()
+       }, [searchQuery])
+
+
+
+    return     entries
+
+    //.filter (entry => entry.text.toUpperCase().includes(searchQuery.toUpperCase())) /* toUpperCase w dwóch miejscach tak aby wyszukiwanie pomijało wielkość liter, w tym przypadku wszystko formatujemy do dużych liter i elo, można też dać do małych */
 }
